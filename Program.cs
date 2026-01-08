b949328c21c57b1fe84f6465f718a8964e81f137
suhaaanii <suhanisaluja05@gmail.com>
2026-01-08 18:22:43 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Whos this");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + " !");
+
+            Console.WriteLine("Whats ur age?");
+            string ageInput = Console.ReadLine();
+            int age;
+            bool isValidAge = int.TryParse(ageInput, out age);
+            if (isValidAge)
+            {
+                if (age>=18)
+                {
+                    Console.WriteLine(" Can Vote");
+                }
+                else
+                {
+                    Console.WriteLine(" Can't Vote");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Error invalid");
+            }
+
+            }
+    }
+}
