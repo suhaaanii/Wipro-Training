6d03b1cb0b3d718b4c55713b05353f9421e9ed28
suhaaanii <suhanisaluja05@gmail.com>
2026-01-14 09:32:55 +0530

    Day5

---
+++ Day5/ConsoleAppDay5/Program.cs
@@ -0,0 +1,17 @@
+﻿//creating a file using File.Create() method in default directory
+string filePath = "demo.txt";
+using (FileStream fs = File.Create(filePath))
+{
+    // File created successfully
+    if (File.Exists(filePath))
+    {
+        Console.WriteLine("File created successfully: " + filePath);
+    }
+}
+
+//Writing to the file using StreamWriter class
+using (StreamWriter sw = new StreamWriter(filePath))
+{
+    sw.WriteLine("Hello, this is a demo file created today.");
+    sw.WriteLine("This file is created to demonstrate file handling in C#.");
+}
